2bc979d6b7ec1be3e48386cfeb52f0b70b168d77
Aziz Ahmad Khan <aziz.ahmad.khan978@gmail.com>
2026-08-09 17:09:41 +0500

    Configure services for AWS deployment

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -4,13 +4,14 @@ const app = require("./app");
 const { testDatabaseConnection } = require("./config/database");
 
 const port = process.env.PORT || 3001;
+const host = "0.0.0.0";
 
 async function startServer() {
   try {
     await testDatabaseConnection();
 
-    app.listen(port, () => {
-      console.log(`Auth Service running on http://localhost:${port}`);
+    app.listen(port,host, () => {
+      console.log(`Auth Service running on http://${host}:${port}`);
     });
   } catch (error) {
     console.error("Failed to start Auth Service:", error.message);
--- resource-service/src/server.js
+++ resource-service/src/server.js
@@ -4,13 +4,14 @@ const app = require("./app");
 const { testDatabaseConnection } = require("./config/database");
 
 const port = Number(process.env.PORT) || 3002;
+const host = "0.0.0.0";
 
 async function startServer() {
   try {
     await testDatabaseConnection();
 
-    app.listen(port, () => {
-      console.log(`Resource Service running on http://localhost:${port}`);
+    app.listen(port, host, () => {
+      console.log(`Resource Service running on http://${host}:${port}`);
     });
   } catch (error) {
     console.error("Failed to start Resource Service:", error);
